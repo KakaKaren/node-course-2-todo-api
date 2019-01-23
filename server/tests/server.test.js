5a1aac09d1ad499a2bb99d7f0247af8710240ea2
KakaKaren <karen.isaga.arias@gmail.com>
2019-01-23 11:16:39 -0500

    Add GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require ('expect');
 const request = require('supertest');
 
+
 const {app} = require("./../server");
 const{Todo} = require("./../models/todo");
 
 const todos = [{
+   
     text:"First test todo"
 },{
+    
     text:"Second test todo"
 }];
 
@@ -76,3 +79,5 @@ describe("POST /todos",()=>
           .end(done);
         });
     });
+
+   
